f8cb08b7bf8724bd2e10abd4ead67fce25714754
bettercodevivek <viveksingh0520@gmail.com>
2024-09-03 16:25:00 +0530

    Creation of new file

--- Vevy/src/App.jsx
+++ Vevy/src/App.jsx
@@ -4,7 +4,7 @@ import React from "react";
 const App = () =>{
   return(
         <React.Fragment>
-           <h1 className="text-5xl text-center">helo</h1>
+          
         </React.Fragment>
   );
 }
