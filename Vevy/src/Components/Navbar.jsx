0be02b6f4f6a3edef80f817e6848fa1052cc1a54
bettercodevivek <viveksingh0520@gmail.com>
2024-09-06 09:38:01 +0530

    Updation in navbar, finalised design

--- Vevy/src/Components/Navbar.jsx
+++ Vevy/src/Components/Navbar.jsx
@@ -1,40 +1,34 @@
-// src/Navbar.jsx
 
 import React from 'react';
 
 const Navbar = () => {
   return (
-    <nav className="bg-gradient-to-r from-slate-300 to-slate-400 sticky w-full z-50 top-0 left-0 px-8">
-      <div className="container mx-auto px-4 py-2 flex flex-col justify-center items-center gap-2">
+    <nav className="bg-black text-white shadow-lg sticky w-full z-50 top-0 left-0 px-24">
+      <div className="container mx-auto px-4 py-4 flex justify-between items-center">
         {/* Logo */}
-        <div className="text-3xl font-bold font-sans tracking-wider flex flex-col items-center justify-center">
-          <p>
-            VWS
-          </p>
-          <p className='text-sm font-normal pl-1'>
-            Vevy Web Solutions
-          </p>
-        </div>
+       <div>
+        <p className='tracking-wider text-2xl font-bold'>VWS</p>
+        <p className='font-normal text-sm'>Vevy Web Solutions</p>
+       </div>
 
         {/* Navigation Links */}
-        <div className="hidden md:flex space-x-12 text-lg font-semibold">
-  <a href="#Home" className="text-black hover:text-blue-600 hover:underline hover:decoration-1 hover:underline-offset-4">
-    HOME
-  </a>
-  <a href="#Services" className="text-black hover:text-blue-600 hover:underline hover:decoration-1 hover:underline-offset-4">
-    OUR SERVICES
-  </a>
-  <a href="#Technology" className="text-black hover:text-blue-600 hover:underline hover:decoration-1 hover:underline-offset-4">
-    OUR TECHNOLOGY
-  </a>
-  <a href="#Testimonials" className="text-black hover:text-blue-600 hover:underline hover:decoration-1 hover:underline-offset-4">
-    OUR TESTIMONIALS
-  </a>
-  <a href="#Contact" className="text-black hover:text-blue-600 hover:underline hover:decoration-1 hover:underline-offset-4">
-    CONTACT US
-  </a>
-</div>
-
+        <div className="space-x-6">
+          <a href="#Home" className="text-white hover:text-blue-600 font-medium">
+            Home
+          </a>
+          <a href="#Services" className="text-white hover:text-blue-600 font-medium">
+            Our Services
+          </a>
+          <a href="#Technology" className="text-white hover:text-blue-600 font-medium">
+            Our Technology
+          </a>
+          <a href="#Testimonials" className="text-white hover:text-blue-600 font-medium">
+            Our Testimonials
+          </a>
+          <a href="#Contact" className="text-white hover:text-blue-600 font-medium">
+            Contact Us
+          </a>
+        </div>
       </div>
     </nav>
   );
