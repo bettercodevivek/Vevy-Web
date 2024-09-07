d33e9204951cb22250ff3d8199a52e6ce732a546
bettercodevivek <viveksingh0520@gmail.com>
2024-09-07 14:04:20 +0530

    disable PIP on browsers

--- Vevy/src/Components/Landing.jsx
+++ Vevy/src/Components/Landing.jsx
@@ -56,7 +56,7 @@ const LandingPage = () => {
             <div className="flex flex-col items-start justify-start sm:items-center sm:justify-center sm:flex-row sm:space-x-8 mt-4 sm:mt-0">
               <div className="bg-white bg-opacity-30 p-2 sm:p-4 backdrop-blur-lg shadow-xl rounded-xl flex-shrink-0 w-full md:w-3/4 lg:w-2/3 mb-4 sm:mb-0">
                 <div className="relative pb-9/16">
-                  <video autoPlay loop muted className="w-full h-full object-cover rounded-lg">
+                  <video disablePictureInPicture autoPlay loop muted className="w-full h-full object-cover rounded-lg">
                     <source src="/Demo_video.mp4" type="video/mp4" />
                     Your browser does not support the video tag.
                   </video>
@@ -66,7 +66,7 @@ const LandingPage = () => {
 
               <div className="bg-white bg-opacity-30 p-2 sm:p-4 backdrop-blur-lg rounded-xl shadow-xl flex-shrink-0">
                 <div className="relative pb-9/16">
-                  <video autoPlay loop muted className="xl:h-96 lg:h-80 md:h-72 sm:h-60 h-96 object-cover rounded-lg">
+                  <video disablePictureInPicture autoPlay loop muted className="xl:h-96 lg:h-80 md:h-72 sm:h-60 h-96 object-cover rounded-lg">
                     <source src="/Demo_web_mobile.mp4" type="video/mp4" />
                     Your browser does not support the video tag.
                   </video>
@@ -89,7 +89,7 @@ const LandingPage = () => {
               
               <div className="bg-white bg-opacity-30 p-2 sm:p-4 backdrop-blur-lg rounded-xl shadow-xl flex-shrink-0 mb-8 sm:mb-0">
                 <div className="relative pb-9/16">
-                  <video autoPlay loop muted className="xl:h-96 lg:h-80 md:h-72 sm:h-60 h-96 object-cover rounded-lg">
+                  <video disablePictureInPicture autoPlay loop muted className="xl:h-96 lg:h-80 md:h-72 sm:h-60 h-96 object-cover rounded-lg">
                     <source src="/Demo_mobile_2.mp4" type="video/mp4" /> 
                     Your browser does not support the video tag.
                   </video>
@@ -99,7 +99,7 @@ const LandingPage = () => {
 
               <div className="bg-white bg-opacity-30 p-2 sm:p-4 backdrop-blur-lg shadow-xl rounded-xl flex-shrink-0 w-full md:w-3/4 lg:w-2/3">
                 <div className="relative pb-9/16">
-                  <video autoPlay loop muted className="w-full h-full object-cover rounded-lg">
+                  <video disablePictureInPicture autoPlay loop muted className="w-full h-full object-cover rounded-lg">
                     <source src="/Demo2New.mp4" type="video/mp4" />
                     Your browser does not support the video tag.
                   </video>
